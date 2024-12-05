72af6167526d81bfe0b6568836301aa4b25923a9
Gabriel Rogi <52065970+Izatomo@users.noreply.github.com>
2024-12-05 20:05:42 +0100

    .

--- AP_Searchy/app.py
+++ AP_Searchy/app.py
@@ -1,18 +1,48 @@
+import os
 import sqlite3
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+DATABASE = 'knowledge_base.db'
+
+def get_db_connection():
+    conn = sqlite3.connect(DATABASE)
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def init_db():
+    if not os.path.exists(DATABASE):
+        conn = sqlite3.connect(DATABASE)
+        c = conn.cursor()
+        c.execute('''CREATE TABLE IF NOT EXISTS papers (
+                        id INTEGER PRIMARY KEY,
+                        content TEXT,
+                        directory_name TEXT
+                    )''')
+        c.execute('''CREATE TABLE IF NOT EXISTS hashtags (
+                        id INTEGER PRIMARY KEY,
+                        paper_id INTEGER,
+                        hashtag TEXT,
+                        FOREIGN KEY (paper_id) REFERENCES papers (id)
+                    )''')
+        c.execute('''CREATE TABLE IF NOT EXISTS images (
+                        id INTEGER PRIMARY KEY,
+                        paper_id INTEGER,
+                        image_data TEXT,
+                        FOREIGN KEY (paper_id) REFERENCES papers (id)
+                    )''')
+        conn.commit()
+        conn.close()
+
 @app.route('/')
 def index():
     conn = get_db_connection()
     cur = conn.cursor()
-    cur.execute('SELECT content FROM papers LIMIT 1')  # Get the first entry from 'papers'
+    cur.execute('SELECT content FROM papers LIMIT 1')
     result = cur.fetchone()
-    if result:
-        result = result[0]
-    else:
-        result = "No content found in the database"
+    conn.close()
+    result = result[0] if result else "Keine Inhalte gefunden."
     return render_template('searchy.html', result=result)
 
 @app.route('/search', methods=['POST'])
@@ -22,18 +52,10 @@ def search():
     cur = conn.cursor()
     cur.execute("SELECT content FROM papers WHERE content LIKE ?", ('%' + query + '%',))
     result = cur.fetchone()
-    if result:
-        result = result[0]
-    else:
-        result = "No content found for the search query"
+    conn.close()
+    result = result[0] if result else "Keine Inhalte gefunden."
     return render_template('searchy.html', result=result)
 
-def get_db_connection():
-    conn = sqlite3.connect('knowledge_base.db')
-    conn.row_factory = sqlite3.Row
-    return conn
-
 if __name__ == '__main__':
-    from db_module import init_db
-    init_db()  # Initialize the database on app startup
+    init_db()
     app.run(debug=True)
